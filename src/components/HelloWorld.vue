eff6f6ed17698c4e4b9d2d6a0182a4710770c0da
Vinícius Tadeu Andrade Costa <45463783+ViniciusTAC@users.noreply.github.com>
2024-12-23 16:25:52 -0300

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,11 +2,11 @@
   <v-app>
     <v-container>
       <v-carousel
-        cycle
-        height="95vh"
-        width= "100vw"
+        v-model="currentSlide"
+        height="90vh"
         hide-delimiters
-        show-arrows-on-hover
+        :show-arrows="false"
+        class="custom-carousel"
       >
         <v-carousel-item
           v-for="(image, index) in images"
@@ -18,11 +18,20 @@
             contain
             class="carousel-image"
           ></v-img>
-          <div class="caption">
+          <!-- <div class="caption">
             <strong>{{ image.alt }}</strong>
-          </div>
+          </div> -->
         </v-carousel-item>
       </v-carousel>
+
+      <!-- Navigation buttons -->
+      <div class="carousel-controls">
+        <v-btn @click="prevSlide">Anterior</v-btn>
+        <v-btn v-for="(image, index) in images" :key="index" @click="goToSlide(index)">
+          {{ index + 1 }}
+        </v-btn>
+        <v-btn @click="nextSlide">Proximo</v-btn>
+      </div>
     </v-container>
   </v-app>
 </template>
@@ -31,6 +40,7 @@
 export default {
   data() {
     return {
+      currentSlide: 0,
       images: [
         { src: require('@/assets/slide1.png'), alt: 'Slide 1' },
         { src: require('@/assets/slide2.png'), alt: 'Slide 2' },
@@ -43,6 +53,17 @@ export default {
       ],
     };
   },
+  methods: {
+    prevSlide() {
+      this.currentSlide = (this.currentSlide - 1 + this.images.length) % this.images.length;
+    },
+    nextSlide() {
+      this.currentSlide = (this.currentSlide + 1) % this.images.length;
+    },
+    goToSlide(index) {
+      this.currentSlide = index;
+    },
+  },
 };
 </script>
 
@@ -52,12 +73,23 @@ html {
 }
 
 .carousel-image {
-  height: 90vh;
+  height: 100vh;
 }
 
 .caption {
   text-align: center;
   font-size: 18px;
   margin-top: 10px;
+}
+
+.carousel-controls {
+  display: flex;
+  justify-content: center;
+  /* margin-top: 20px; */
+  /* gap: 10px; */
+}
+
+.carousel-controls v-btn {
+  min-width: 40px;
 }
 </style>
